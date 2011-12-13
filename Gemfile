2a460648aa5cab0670eb64354f4cbf8151463988
Andrew Kasper <kasper@vitrue.com>
2011-12-12 19:24:01 -0500

    [NEW] Bare-bones first pass. Built on postgres, using DataMapper and Haml. Sidenote: this should be the default for Rails apps.

---
+++ Gemfile
@@ -0,0 +1,18 @@
+#! /bin/ruby
+
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+
+#models via Datamapper
+gem 'dm-rails'
+gem 'dm-postgres-adapter'
+gem 'dm-migrations'
+
+#views via Haml and SASS
+gem 'haml-rails'
+
+group :test do
+  gem 'rspec-rails'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gluespot::Application.config.secret_token = 'cc458e8194bfd25f6a1763499012ab1da35589df315ec6fa0b2dbc04f2c0959b4e8bb7ec452bf0eca02dc1ec8015efa6a1c35fcfd7a6128e33133b14608e5945'
